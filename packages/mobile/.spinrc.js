e4dc6590743ea2e846dc53c012e02d30b1cf6f43
Ani <ani.fullstack@gmail.com>
2018-03-01 16:46:53 -0700

    ausk initial clone. tested on all devices. working good (except for a glitch

---
+++ packages/mobile/.spinrc.js
@@ -0,0 +1,61 @@
+const url = require("url");
+
+const config = {
+  builders: {
+    android: {
+      entry: "./src/index.ts",
+      buildDir: "build/android",
+      dllBuildDir: "build/android/dll",
+      stack: ["react-native", "android"],
+      defines: {
+        __CLIENT__: true
+      },
+      enabled: true
+    },
+    ios: {
+      entry: "./src/index.ts",
+      buildDir: "build/ios",
+      dllBuildDir: "build/ios/dll",
+      stack: ["react-native", "ios"],
+      defines: {
+        __CLIENT__: true
+      },
+      enabled: true
+    },
+    test: {
+      stack: ["react-native-web", "server"],
+      roles: ["test"],
+      defines: {
+        __TEST__: true
+      }
+    }
+  },
+  options: {
+    stack: ["apollo", "react", "styled-components", "es6", "ts", "webpack"],
+    cache: "../../.cache",
+    webpackDll: true,
+    reactHotLoader: false,
+    persistGraphQL: false,
+    defines: {
+      __DEV__: process.env.NODE_ENV !== "production",
+      __BACKEND_URL__: '"http://localhost:8080/graphql"'
+    }
+  }
+};
+
+if (process.env.NODE_ENV === "production") {
+  config.builders.android.enabled = true;
+  config.builders.ios.enabled = true;
+  config.options.defines.__BACKEND_URL__ =
+    '"https://apollo-universal-starter-kit.herokuapp.com/graphql"';
+  // Generating source maps for production will slowdown compilation for roughly 25%
+  config.options.sourceMap = false;
+}
+
+const extraDefines = {
+  __PERSIST_GQL__: config.options.persistGraphQL
+};
+
+config.options.defines = Object.assign(config.options.defines, extraDefines);
+
+module.exports = config;
